6e8461fc13f923604c781a9ad3a8a8d0e51330e9
gitcjm <163mail-cjm@163.com>
2016-04-13 11:32:42 +0800

    delete a file

--- shenyi.c
+++ shenyi.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
+int get_age() {
+  return 18;
+}
+
 int main(){
-  printf("Hello,cjm. This is my memery of c.\n");
+  int age = get_age();
+  printf("My age is %d\n", age);
+  // printf("Hello cjm\n");
   return 0;
 }
